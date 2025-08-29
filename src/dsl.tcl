5e4733291a4f307a416a745b2e9c0658bc162898
Brennan Holten <bholten@protonmail.ch>
2025-08-29 01:35:45 -0500

    Fix tabs/spaces inconsistencies

--- src/dsl.tcl
+++ src/dsl.tcl
@@ -2,30 +2,30 @@ namespace eval ::flux::__internal {
     variable uid 0
 
     proc gensym {{prefix "n"}} {
-	variable uid
-	incr uid
-	return "${prefix}${uid}"
+        variable uid
+        incr uid
+        return "${prefix}${uid}"
     }
 
     proc safe_name {s} {
-	set s [string tolower $s]
-	regsub -all {[^a-z0-9_]+} $s {_} s
-	return $s
+        set s [string tolower $s]
+        regsub -all {[^a-z0-9_]+} $s {_} s
+        return $s
     }
 
     proc split_key_values {value_string} {
-	set values [split ${value_string} "\n"]
+        set values [split ${value_string} "\n"]
 
-	set trim_configs [lmap config $values {
-	    set c [string trim $config]
-	    if {![string equal $c ""]} {
-		set config $c
-	    } else {
-		continue
-	    }
-	}]
+        set trim_configs [lmap config $values {
+            set c [string trim $config]
+            if {![string equal $c ""]} {
+                set config $c
+            } else {
+                continue
+            }
+    }]
 
-	return $trim_configs
+        return $trim_configs
     }
 }
 
@@ -37,123 +37,123 @@ namespace eval ::flux {
     set steps {}
 
     proc headers {hds} {
-	variable request
+        variable request
 
-	set hs [split $hds "\n"]
-	set hs [lmap h $hs {
-	    set entry [string trim $h]
-	    if {[string equal $entry ""]} {
-		continue
-	    }
+        set hs [split $hds "\n"]
+        set hs [lmap h $hs {
+            set entry [string trim $h]
+            if {[string equal $entry ""]} {
+                continue
+            }
 
-	    set entry [string trim [subst $entry]]
-	    set h $entry
-	}]
+            set entry [string trim [subst $entry]]
+            set h $entry
+        }]
 
-	set request [dict set request headers $hs]
+        set request [dict set request headers $hs]
     }
 
     proc options {opts} {
-	variable request
+        variable request
 
-	set os [list {*}[subst $opts]]
-	set os [lmap o $os {
-	    set entry [string trim $o]
-	    if {[string equal $entry ""]} {
-		continue
-	    }
+        set os [list {*}[subst $opts]]
+        set os [lmap o $os {
+            set entry [string trim $o]
+            if {[string equal $entry ""]} {
+                continue
+            }
 
-	    set entry [string trim $entry ":"]
-	    set o $entry
-	}]
+            set entry [string trim $entry ":"]
+            set o $entry
+        }]
 
-	set request [dict set request options $os]
+        set request [dict set request options $os]
     }
 
     proc data {d} {
-	variable request
-	set request [dict set request data $d]
+        variable request
+        set request [dict set request data $d]
     }
 
     proc http_request {verb url body} {
-	variable request
-	variable steps
+        variable request
+        variable steps
 
-	set request {}
+        set request {}
 
-	set request_namespace [format "%s::%s" \
-				   [namespace current] \
-				   [::flux::__internal::gensym req_]]
+        set request_namespace [format "%s::%s" \
+                                   [namespace current] \
+                                   [::flux::__internal::gensym req_]]
 
-	uplevel 1 $body
+        uplevel 1 $body
 
-	set entry [dict create \
-		       ns $request_namespace \
-		       verb $verb \
-		       url $url \
-		       request $request]
+        set entry [dict create \
+                       ns $request_namespace \
+                       verb $verb \
+                       url $url \
+                       request $request]
 
-	lappend steps $entry
+        lappend steps $entry
 
-	return $entry
+        return $entry
     }
 
 
     proc GET {url body} {
-	return [http_request GET $url $body]
+        return [http_request GET $url $body]
     }
 
     proc POST {url body} {
-	return [http_request POST $url $body]
+        return [http_request POST $url $body]
     }
 
     proc PUT {url body} {
-	return [http_request PUT $url $body]
+        return [http_request PUT $url $body]
     }
 
     proc DELETE {url body} {
-	return [http_request DELETE $url $body]
+        return [http_request DELETE $url $body]
     }
 
     proc PATCH {url body} {
-	return [http_request PATCH $url $body]
+        return [http_request PATCH $url $body]
     }
 
     proc OPTIONS {url body} {
-	return [http_request OPTIONS $url $body]
+        return [http_request OPTIONS $url $body]
     }
 
     proc run {} {
-	variable steps
+        variable steps
 
-	foreach req $steps {
-	    set h [flux.init]
+        foreach req $steps {
+            set h [flux.init]
 
-	    $h configure -url [dict get $req url]
-	    $h configure -verb [dict get $req verb]
+            $h configure -url [dict get $req url]
+            $h configure -verb [dict get $req verb]
 
-	    if {[dict exists $req request]} {
-		set r [dict get $req request]
+            if {[dict exists $req request]} {
+                set r [dict get $req request]
 
-		if {[dict exists $r options]} {
-		    dict for {key value} [dict get $r options] {
-			$h configure -$key $value
-		    }
-		}
+                if {[dict exists $r options]} {
+                    dict for {key value} [dict get $r options] {
+                        $h configure -$key $value
+                    }
+                }
 
-		if {[dict exists $r headers]} {
-		    foreach {key} [dict get $r headers] {
-			$h header $key
-		    }
-		}
+                if {[dict exists $r headers]} {
+                    foreach {key} [dict get $r headers] {
+                        $h header $key
+                    }
+                }
 
-		if {[dict exists $r data]} {
-		    $h body [dict get $r data]
-		}
-	    }
+                if {[dict exists $r data]} {
+                    $h body [dict get $r data]
+                }
+            }
 
-	    $h perform
-	    $h reset
-	}
+            $h perform
+            $h reset
+        }
     }
 }
